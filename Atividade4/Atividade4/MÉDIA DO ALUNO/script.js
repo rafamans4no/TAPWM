d52141bc8fd92553934f14d3fcca94381caf5eaf
Rafaela Mansano <102178628+rafamans4no@users.noreply.github.com>
2025-04-09 15:27:31 -0300

    Delete Atividade4/Atividade4 directory

--- Atividade4/Atividade4/MÉDIA DO ALUNO/script.js
+++
@@ -1,9 +0,0 @@
-function Media() {
-    let nome = prompt("Digite o nome do aluno:");
-    let nota1 = parseFloat(prompt("Digite a primeira nota:"));
-    let nota2 = parseFloat(prompt("Digite a segunda nota:"));
-    let nota3 = parseFloat(prompt("Digite a terceira nota:"));
-    
-    let media = (nota1 + nota2 + nota3) / 3;
-    alert(`Aluno: ${nome}\nMédia: ${media.toFixed(2)}`);
-}
